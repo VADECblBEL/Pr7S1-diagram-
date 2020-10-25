c279b882109976600e4e56345d32013a0ad49097
Вадим <vadimmm.m@ya.ru>
2020-10-25 14:15:49 +0300

    .

--- PieChart/Form1.cs
+++ PieChart/Form1.cs
@@ -36,11 +36,23 @@ namespace PieChart
 
         private Image drawPieChart(ArrayList elements, Size s)
         {
+            Color[] colors = { Color.Red, Color.Violet, Color.Aqua, Color.FromArgb(0xAA00FF),
+                Color.Lavender, Color.Indigo, Color.DarkSalmon, Color.DarkRed,
+                Color.DarkOrange, Color.DarkSalmon, Color.DarkGreen,
+                Color.DarkBlue, Color.Lavender, Color.LightBlue, Color.Coral };
+
+            if (elements.Count > colors.Length)
+            {
+                throw new ArgumentException("Pie chart must have " + colors.Length + " or fewer elements");
+            }
+
             Bitmap bm = new Bitmap(s.Width, s.Height);
             Graphics g = Graphics.FromImage(bm);
+            g.SmoothingMode = SmoothingMode.HighQuality;
 
-            float total = 0;
             
+            float total = 0;
+
             foreach (PieChartElement e in elements)
             {
                 if (e.value < 0)
@@ -49,27 +61,37 @@ namespace PieChart
                 }
                 total += e.value;
             }
-            
+
             if (!(total > 0))
             {
                 throw new ArgumentException("Must provide at least one PieChartElement with a positive value");
             }
 
+            
             Rectangle rect = new Rectangle(1, 1, s.Width - 2, s.Height - 2);
-            Pen p = new Pen(Color.Red, 1);
 
+            Pen p = new Pen(Color.Black, 5);
+
+            
             float startAngle = 0;
-        
+            int colorNum = 0;
+
+            
             foreach (PieChartElement e in elements)
             {
                 
+                Brush b = new LinearGradientBrush(rect, colors[colorNum++], Color.White, (float)45);
                 float sweepAngle = (e.value / total) * 360;
+                g.FillPie(b, rect, startAngle, sweepAngle);
                 g.DrawPie(p, rect, startAngle, sweepAngle);
                 startAngle += sweepAngle;
             }
-
             return bm;
-            
+        }
+
+        private void chart_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
